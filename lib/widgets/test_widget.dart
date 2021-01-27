cef9712b24e7abe48c7ccf92ff534ef052bfd61c
mskarkala <meskozani@gmail.com>
2021-01-27 10:47:10 +0100

    add widget tests

---
+++ lib/widgets/test_widget.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+// https://flutter.dev/docs/cookbook/testing/widget/introduction
+class MyWidget extends StatelessWidget {
+  final String title;
+  final String message;
+
+  const MyWidget({
+    Key key,
+    @required this.title,
+    @required this.message,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(title),
+        ),
+        body: Center(
+          child: Text(message),
+        ),
+      ),
+    );
+  }
+}
