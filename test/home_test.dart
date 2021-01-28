298fcc7b1119f6851de5d588e867cd7241c5957d
Jochen Pfeiffer <git@jochen-pfeiffer.com>
2021-01-28 13:59:23 +0100

    revert home_page.dart to original state

--- test/home_test.dart
+++ test/home_test.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_starter/main.dart';
 import 'package:flutter_test/flutter_test.dart';
+
+import 'package:flutter_starter/main.dart';
 
 void main() {
   _testCounter();
